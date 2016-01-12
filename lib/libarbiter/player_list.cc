3c00a84613d61ef2993eaea990fb85d9ac7a374c
Terry Bolt <bolt.terry@gmail.com>
2016-01-12 15:55:10 +0000

    Refactored factory functions to use named constructors. Also fixed several bugs.

--- lib/libarbiter/player_list.cc
+++ lib/libarbiter/player_list.cc
@@ -2,6 +2,7 @@
 
 #include <fstream>
 #include <mutex>
+#include <stdexcept>
 
 #include <boost/log/trivial.hpp>
 #include <boost/property_tree/json_parser.hpp>
@@ -9,74 +10,78 @@
 #include <boost/uuid/uuid.hpp>
 
 #include "player.h"
+#include "util.h"
 
 namespace libarbiter {
 
-void PlayerList::Add(Player* player) {
+void PlayerList::Add(const Player& player) {
   std::lock_guard<std::mutex> lock(players_lock_);
-  players_.emplace(player->id(), std::unique_ptr<Player>(player));
+  players_.emplace(player.id(), player);
 }
 
-void PlayerList::Remove(boost::uuids::uuid id) {
+void PlayerList::Remove(const boost::uuids::uuid& id) {
   std::lock_guard<std::mutex> lock(players_lock_);
   players_.erase(id);
 }
 
-const Player* PlayerList::GetPlayer(boost::uuids::uuid id) const {
+const Player* PlayerList::GetPlayer(const boost::uuids::uuid& id) const {
   auto player_pair = players_.find(id);
   if (player_pair != players_.end()) {
-    return player_pair->second.get();
+    return &(player_pair->second);
   }
   return nullptr;
 }
 
-Player* PlayerList::GetMutablePlayer(boost::uuids::uuid id) {
+Player* PlayerList::GetMutablePlayer(const boost::uuids::uuid& id) {
   auto player_pair = players_.find(id);
   if (player_pair != players_.end()) {
-    return player_pair->second.get();
+    return &(player_pair->second);
   }
   return nullptr;
 }
 
 int PlayerList::NumPlayers() const { return players_.size(); }
 
-bool PlayerList::PopulateFromFile(std::string path, PlayerStringType type) {
+void PlayerList::PopulateFromFile(std::string path, PlayerStringType type) {
   switch (type) {
     case PlayerStringType::CSV:
-      return PopulateFromCSVFile(path);
+      PopulateFromCSVFile(path);
+      break;
     case PlayerStringType::JSON:
-      return PopulateFromJSONFile(path);
+      PopulateFromJSONFile(path);
+      break;
   }
-  return false;
 }
 
-bool PlayerList::PopulateFromCSVFile(std::string path) {
+void PlayerList::PopulateFromCSVFile(std::string path) {
   std::ifstream file(path);
   if (!file.is_open()) {
     BOOST_LOG_TRIVIAL(warning) << "File " << path << " failed to open.";
-    return false;
+    throw FileNotFoundException("File " + path + " failed to open.");
   }
   std::string line;
   while (std::getline(file, line)) {
-    Add(Player::NewPlayer(line, PlayerStringType::CSV));
+    Add(Player::FromCSV(line));
   }
-  return true;
 }
 
-bool PlayerList::PopulateFromJSONFile(std::string path) {
+void PlayerList::PopulateFromJSONFile(std::string path) {
   std::ifstream file(path);
   if (!file.is_open()) {
     BOOST_LOG_TRIVIAL(warning) << "File " << path << " failed to open.";
-    return false;
+    throw FileNotFoundException("File " + path + " failed to open.");
   }
   boost::property_tree::ptree tree;
-  boost::property_tree::read_json(file, tree);
+  try {
+    boost::property_tree::read_json(file, tree);
+  } catch (std::exception&) {
+    throw InvalidJSONException();
+  }
   for (const auto player : tree.get_child("players")) {
     std::stringstream player_ss;
     boost::property_tree::write_json(player_ss, player.second, false);
-    Add(Player::NewPlayer(player_ss.str(), PlayerStringType::JSON));
+    Add(Player::FromJSON(player_ss.str()));
   }
-  return true;
 }
 
 }  // namespace libarbiter
